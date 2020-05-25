531f9eeba0bc0065eaaf2f1e7d948d42b60dff18
zxb139076 <50727140+zxb139076@users.noreply.github.com>
2020-05-25 11:52:06 +0800

    小修改
    小修改

--- miniprogram/pages/courseArrange/courseArrange.js
+++ miniprogram/pages/courseArrange/courseArrange.js
@@ -7,23 +7,97 @@ import {
 } from '../util/util.js';
 Page({
   data: {
+    hiddenmodalput: true,
     id: '1',
-    dataList: []
-  },
-
-  onLoad: function (options) {
-    
+    dataList: [],
+    index: 0,
+    time: '',
+    week: '',
   },
 
   onReady: function () {
     var time = formatDate(new Date());
     var date = getDates(7, time);
-    console.log(date);
     this.setData({
       dataList: date,
     })
+    this.setData({
+      week: this.data.dataList[0].week,
+      time: this.data.dataList[0].time
+    })
   },
 
+  dataSelect: function (e) {
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+      week: this.data.dataList[index].week,
+      time: this.data.dataList[index].time,
+      index: index
+    })
+  },
+  
+  // 显示项目添加面板
+  modalinput: function () {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput
+    })
+  },
 
+  //取消按钮 
+  cancel: function () {
+    this.setData({
+      hiddenmodalput: true
+    });
+  },
+
+  //取消按钮 
+  cancel: function () {
+    this.setData({
+      hiddenmodalput: true
+    });
+  },
+  //确认 
+  confirm: function () {
+    if (this.data.projectName == '') {
+      wx.showToast({
+        title: '请填写项目名称',
+      });
+      return false;
+    }
+    if (this.data.projectDetail == '') {
+      wx.showToast({
+        title: '请填写项目描述',
+      });
+      return false;
+    }
+    if (this.data.projectGroups == '') {
+      wx.showToast({
+        title: '请填写适合人群',
+      });
+      return false;
+    }
+    wx.cloud.callFunction({
+      name: "catalogue",
+      data: {
+        requestType: 'catalogueAdd',
+        projectName: this.data.projectName,
+        projectDetail: this.data.projectDetail,
+        projectGroups: this.data.projectGroups
+      }
+    }).then(res => {
+      this.setData({
+        hiddenmodalput: true,
+        projectName: '',
+        projectDetail: '',
+        projectGroups: '',
+      });
+      this.cataLogueList();
+      wx.showToast({
+        title: '新增课程成功',
+      });
+    }).catch(err => {
+      console.error(err)
+    });
+  },
 
 })
--- miniprogram/pages/util/util.js
+++ miniprogram/pages/util/util.js
@@ -3,9 +3,9 @@ const formatDate = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
-  const hour = new Date().getHours()
-  const minute = new Date().getMinutes()
-  const second = new Date().getSeconds()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 
 }
