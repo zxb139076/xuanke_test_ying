864209ee15b6c841e3990bf87cfd8bf587220ce5
zxb139076 <50727140+zxb139076@users.noreply.github.com>
2020-05-27 18:03:02 +0800

    准备完善课程预约功能
    准备完善课程预约功能

--- miniprogram/pages/courseReserve/courseReserve.js
+++ miniprogram/pages/courseReserve/courseReserve.js
@@ -1,70 +1,113 @@
-// miniprogram/pages/addCourse/addCourse.js
+// pages/courseArrange/courseArrange.js
 import {
   formatDate
 } from '../util/util.js';
+import {
+  getDates
+} from '../util/util.js';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: '',
-    nickName: '',
-    userAvatarUrl: '',
-    headimgurl: ''
+    dataList: [],
+    index: 0,
+    currentData: '',
+    currentWeek: '',
+    resultList: null,
+    headImage: "https://7875-xuankeying-ykwz0-1256767223.tcb.qcloud.la/catalogue/ipad.jpeg?sign=9184ee1dd0a51f9965bb7fccd2598df3&t=1590470115"
   },
 
-  addCourse: function (e) {
-    wx.getUserInfo({
-      complete: (res) => {
-        this.setData({
-          nickName: res.userInfo.nickName,
-          headimgurl: res.userInfo.avatarUrl
-        })
-        const time = formatDate(new Date());
-        //打印
-        console.log(time)
-        const db = wx.cloud.database()
-        db.collection('courseReserve').add({
-          data: {
-            course: '课程记录',
-            remark: '课程备注',
-            nickName: res.userInfo.nickName,
-            headimgurl: res.userInfo.avatarUrl,
-            updateTime: time,
-          },
-          success: res => {
-            this.setData({
-              counterId: res._id,
-              count: 1
-            })
-            wx.showToast({
-              title: '新增记录成功',
-            })
-            console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
-          },
-          fail: err => {
-            wx.showToast({
-              icon: 'none',
-              title: '新增记录失败'
-            })
-            console.error('[数据库] [新增记录] 失败：', err)
-          }
-        })
-      },
+  // 页面准备渲染
+  onReady: function () {
+    var currentData = null;
+    var currentWeek = null;
+    var time = formatDate(new Date());
+    var dataSet = getDates(7, time);
+    var index = -1;
+    if (this.data.currentData == '') {
+      currentData = dataSet[0].time;
+      currentWeek = dataSet[0].week;
+    } else {
+      currentData = this.data.currentData;
+      currentWeek = this.data.currentWeek;
+    }
+    for (var i = 0; i < dataSet.length; i++) {
+      if (currentData == dataSet[i].time) {
+        index = i;
+        break
+      }
+    }
+    this.setData({
+      dataList: dataSet,
+      currentWeek: currentWeek,
+      currentData: currentData,
+      index: index
     })
-
+    wx.cloud.callFunction({
+      name: "courseArrange",
+      data: {
+        requestType: 'courseArrangeGetList',
+        currentData: this.data.currentData
+      }
+    }).then(res => {
+      this.setData({
+        resultList: res.result.data
+      });
+    }).catch(err => {
+      console.error(err)
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    if (options.currentData != undefined) {
+      this.setData({
+        currentData: options.currentData,
+        currentWeek: options.currentWeek,
+        index: options.index
+      })
+    } else {
+      this.setData({
+        currentData: '',
+        currentWeek: '',
+        index: -1
+      })
+    }
   },
+
+  // 选择当前排课的日期(星期几)
+  dataSelect: function (e) {
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+      currentWeek: this.data.dataList[index].week,
+      currentData: this.data.dataList[index].time,
+      index: index
+    })
+    wx.cloud.callFunction({
+      name: "courseArrange",
+      data: {
+        requestType: 'courseArrangeGetList',
+        currentData: this.data.currentData
+      }
+    }).then(res => {
+      this.setData({
+        resultList: res.result.data
+      });
+    }).catch(err => {
+      console.error(err)
+    })
+  },
+
+  // 显示添加课程排课页面
+  showCourseArrangeAdd: function (event) {
+    wx.navigateTo({
+      url: '../courseArrangeEdit/courseArrangeEdit?currentData=' + this.data.currentData + "&currentWeek=" + this.data.currentWeek,
+    })
+  },
+
+  // 显示编辑课程排课页面
+  showEditCourseArrange: function (event) {
+    wx.navigateTo({
+      url: '../courseArrangeEdit/courseArrangeEdit?id=' + event.currentTarget.dataset.id,
+    })
+  }
 
 })
