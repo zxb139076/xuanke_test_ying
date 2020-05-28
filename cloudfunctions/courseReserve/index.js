46e623fab0c6fd4f4d27f01591b9d17c60ca3be6
zxb139076 <50727140+zxb139076@users.noreply.github.com>
2020-05-28 13:43:55 +0800

    完善我的预约课程功能
    完善我的预约课程功能

--- cloudfunctions/courseReserve/index.js
+++ cloudfunctions/courseReserve/index.js
@@ -1,8 +1,17 @@
 // 云函数入口文件
-const cloud = require('wx-server-sdk')
-cloud.init()
-const db = cloud.database()
+const cloud = require('wx-server-sdk');
+cloud.init();
+const db = cloud.database();
+const _ = db.command;
+
 // 云函数入口函数
 exports.main = async (event, context) => {
- return await db.collection("courseReserve").get()
+  return await db.collection("courseReserve").aggregate().lookup({
+    from: 'courseArrange',
+    localField: 'applyId',
+    foreignField: '_id',
+    as: 'arrangeInfo'
+  }).match({
+    _openid: event.openid
+  }).end();
 }
--- miniprogram/pages/myCourseReserveList/myCourseReserveList.js
+++ miniprogram/pages/myCourseReserveList/myCourseReserveList.js
@@ -16,32 +16,15 @@ Page({
       })
     }
     wx.cloud.callFunction({
-      name: "courseReserve"
-    }).then(res => {
-      var success = false;
-      for (var i = 0; i < res.result.data.length; i++) {
-        var item = res.result.data[i];
-        wx.cloud.callFunction({
-          name: "courseArrange",
-          data: {
-            requestType: 'getCourseArrangeById',
-            id: item.applyId
-          }
-        }).then(res2 => {
-          item["courseName"] = res2.result.data[0].courseName;
-          res.result.data[i - 1] = item;
-          if (i == res.result.data.length - 1) {
-            success = true;
-          }
-        }).catch(err => {
-          console.error(err)
-        })
+      name: "courseReserve",
+      data: {
+        openid: 'oJEfM4iya6jPgen2M9oV65-WQ8bY'
       }
-
-          this.setData({
-            resultList: res.result.data
-          });
-
+    }).then(res => {
+      console.log("rrrrr" + res.result.list);
+      this.setData({
+        resultList: res.result.list
+      });
     }).catch(err => {
       console.error(err)
     })
