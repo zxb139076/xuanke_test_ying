90aea8d5ba28af69e2a6c98458d96b6848b35dad
zxb139076 <50727140+zxb139076@users.noreply.github.com>
2020-06-08 11:03:08 +0800

    首页js修改
    首页js修改

--- miniprogram/pages/homePage/homePage.js
+++ miniprogram/pages/homePage/homePage.js
@@ -1,4 +1,3 @@
-//index.js
 const app = getApp()
 
 Page({
@@ -17,34 +16,9 @@ Page({
       })
       return
     }
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-          // 获取openid
-          this.onGetOpenid();
-        }
-      }
-    })
+    this.onGetOpenid();
   },
 
-  onGetUserInfo: function (e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
 
   onGetOpenid: function () {
     // 调用云函数
@@ -54,15 +28,9 @@ Page({
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
         app.globalData.openid = res.result.openid
-        // wx.navigateTo({
-        //   url: '../userConsole/userConsole',
-        // })
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
       }
     })
   },
