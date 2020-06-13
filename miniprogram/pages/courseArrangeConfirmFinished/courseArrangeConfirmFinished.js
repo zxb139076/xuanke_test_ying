65892c427367e1e095a9ea4ee6b014fd31b9194e
admin <admin@example.com>
2020-06-13 13:02:05 +0800

    完善确认课程完成后的跳转页面
    完善确认课程完成后的跳转页面

--- miniprogram/pages/courseArrangeConfirmFinished/courseArrangeConfirmFinished.js
+++ miniprogram/pages/courseArrangeConfirmFinished/courseArrangeConfirmFinished.js
@@ -3,9 +3,13 @@ Page({
     id: "0",
     isLoad: false,
     courseInfo: '',
+    resultList: null,
     headImgUrl: "https://7875-xuankeying-ykwz0-1256767223.tcb.qcloud.la/catalogue/ipad.jpeg?sign=97e5614693d26e39f7f91d50980fcb80&t=1590716495"
   },
 
+  /**
+   * onReady
+   */
   onReady: function () {
     wx.showLoading({
       title: '加载中',
@@ -17,16 +21,20 @@ Page({
     });
   },
 
+  /**
+   * onLoad
+   */
   onLoad: function (options) {
     wx.cloud.callFunction({
       name: "courseArrange",
       data: {
-        requestType: "getCourseArrangeById",
+        requestType: "showCourseArrangeDetail",
         id: options.id
       }
     }).then(res => {
       this.setData({
-        courseInfo: res.result.data[0],
+        courseInfo: res.result.list[0],
+        resultList: res.result.list[0].courseReserveList,
         id: options.id,
         isLoad: true
       });
@@ -35,12 +43,18 @@ Page({
     })
   },
 
+  /**
+   * 返回设置界面
+   */
   returnToHome: function () {
     wx.switchTab({
-      url: '../index/index',
+      url: '../my/my',
     });
   },
 
+  /**
+   * 返回课程排课界面
+   */
   returnToList: function () {
     wx.redirectTo({
       url: '../courseArrange/courseArrange?currentData=' + this.data.courseInfo.currentData,
